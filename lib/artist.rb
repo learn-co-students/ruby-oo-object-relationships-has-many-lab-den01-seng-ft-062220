9aae6c61ce87027e4045448154203a14a53b848b
mnavoy4 <mnavoy4@gmail.com>
2020-06-26 15:18:25 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize name
+        @name = name
+        @songs = []
+        @@all << self
+    end
+
+    def add_song song
+        @songs << song
+        song.artist = self
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def add_song_by_name song_name
+        song = Song.new song_name
+        song.artist = self
+    end
+
+    def self.song_count
+        Song.all.count
+    end
+
+end
